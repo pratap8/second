1c021b89a76f22a99cbdf68bf12dbafced1d8619
phool <phool@host.docker.internal>
2024-01-15 22:40:42 +0530

    test

--- src/main/java/com/springboot/AndFrontend/SBwithFrontEnd/controller/FirstController.java
+++ src/main/java/com/springboot/AndFrontend/SBwithFrontEnd/controller/FirstController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.springboot.AndFrontend.SBwithFrontEnd.Dao.FileService;
 
 
-@Controller
+@Controller 
 public class FirstController{
 	
 	@Autowired
